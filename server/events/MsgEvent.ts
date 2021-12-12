12e3fe4642da8d8131fbc59eb9ca6e786c3c1c70
ElayGelbart <elaygelbart@gmail.com>
2021-12-12 12:21:05 +0200

    Server New Dirs

--- server/events/MsgEvent.ts
+++ server/events/MsgEvent.ts
@@ -1,16 +1,7 @@
 import { EventEmitter } from "events";
-import MsgModel from "../models/MsgModel";
 import { mongoClient } from "../server";
 export const MsgEvent = new EventEmitter();
 
-MsgEvent.on("sendInfo", async (res) => {
-  const MSGS = await mongoClient.db("ReactChatRoom").collection("Msgs").find();
-  const USERS = await mongoClient
-    .db("ReactChatRoom")
-    .collection("Users")
-    .find();
-  res.write(`data:${JSON.stringify({ msgs: MSGS, users: USERS })}\n\n`);
-});
 MsgEvent.on("sendNewMsg", async (UserMsgObj) => {
   await mongoClient
     .db("ReactChatRoom")
--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
-import jwt from "jsonwebtoken";
-import mongodb from "mongodb";
-import checkAuthJWT from "./middleware/security/Auth";
+import mongodb, { MongoClient } from "mongodb";
+import ErrorHandler from "./middleware/error/generalErrorHandler";
+import userRouter from "./routes/user";
+import chatRouter from "./routes/chat";
 require("dotenv").config();
 
 const MongoUri = process.env.MONGO_URI || process.argv[2];
-export const mongoClient = new mongodb.MongoClient(MongoUri);
+export const mongoClient = new MongoClient(MongoUri);
 mongoClient
   .connect()
   .then(() => {
@@ -20,83 +21,8 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
-
-const USERS: jwt.JwtPayload[] = [];
-const MSGS: { msgAuthor: string; msgText: string; msgTime: string }[] = [];
-
-server.post(
-  "/chat/new/msg",
-  checkAuthJWT,
-  (
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ): void => {
-    const { msgAuthor, msgText }: { [key: string]: string } = req.body;
-    if (!msgAuthor || !msgText) {
-      next({ status: 400, msg: "Need More Fields" });
-      return;
-    }
-    const userMsgObj = { msgAuthor, msgText, msgTime: new Date() };
-    console.log(userMsgObj, "MSGMSG 4TEST");
-
-    res.send("sucseesed");
-    MsgEvent.emit("sendNewMsg", userMsgObj);
-  }
-);
-
-server.post(
-  "/users/auth",
-  checkAuthJWT,
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    try {
-      const { authorization } = req.headers;
-      if (!authorization) {
-        throw authorization;
-      }
-      const UserJWT = authorization.split(" ")[1];
-      const cookieUserObj = jwt.verify(UserJWT, JWTSALT);
-      if (typeof cookieUserObj === "string") {
-        throw cookieUserObj;
-      }
-      USERS.push(cookieUserObj);
-      res.send(cookieUserObj);
-    } catch (err) {
-      next({ status: 500, msg: "unknown error" });
-    }
-  }
-);
-
-server.post(
-  "/users/login",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    //future more complex login
-    const { username } = req.body;
-    if (!username) {
-      next({ status: 400, msg: "Enter Username" });
-      return;
-    }
-    const JWTcookie: string = jwt.sign({ username: username }, JWTSALT, {
-      expiresIn: "1h",
-    });
-    res.cookie("JWT", JWTcookie, { maxAge: 1021031 });
-    res.send("Login Sucssued");
-  }
-);
-
-server.use(
-  (
-    err: any,
-    _req: express.Request,
-    res: express.Response,
-    _next: express.NextFunction
-  ) => {
-    if (err.status) {
-      res.status(err.status).send(err.msg);
-      return;
-    }
-    res.send(err).status(500);
-  }
-);
+server.use("/user", userRouter);
+server.use("/chat", chatRouter);
+server.use(ErrorHandler);
 
 export default server;
