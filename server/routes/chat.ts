7f1055c8c680a4b954e77e89778805ca57d91216
ElayGelbart <elaygelbart@gmail.com>
2021-12-12 14:17:49 +0200

    remove some manual typing

--- server/routes/chat.ts
+++ server/routes/chat.ts
@@ -7,39 +7,44 @@ import { JWTSALT } from "../secret";
 
 const chatRouter = express.Router();
 
-chatRouter.get(
-  "/stream",
-  async (
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ): Promise<void> => {
-    res.set({
-      "Content-Type": "text/event-stream",
-      Connection: "keep-alive",
-      "Cache-Control": "no-cache",
-      "Access-Control-Allow-Origin": "http://localhost:3000", // change this in prod
-      "Access-Control-Allow-Credentials": "true",
+chatRouter.get("/stream", async (req, res, next): Promise<void> => {
+  res.set({
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+    "Access-Control-Allow-Origin": "http://localhost:3000", // change this in prod
+    "Access-Control-Allow-Credentials": "true",
+  });
+  // Security check
+  const { cookie } = req.headers;
+
+  try {
+    if (!cookie) {
+      throw cookie;
+    }
+    const userCookieJWT = cookie.split("=")[1];
+    const userObj = jwt.verify(userCookieJWT, JWTSALT);
+    console.log(userObj, "user Obj 4test");
+
+    if (typeof userObj === "string") {
+      throw userObj;
+    }
+    MsgEvent.emit("sendNewMsg", {
+      msgAuthor: "Server",
+      msgText: `${userObj.username} Connected`,
+      msgTime: new Date(),
     });
-    // Security check
-    const { cookie } = req.headers;
-
-    try {
-      if (!cookie) {
-        throw cookie;
-      }
-      const userCookieJWT = cookie.split("=")[1];
-      const userObj = jwt.verify(userCookieJWT, JWTSALT);
-      console.log(userObj, "user Obj 4test");
-
-      if (typeof userObj === "string") {
-        throw userObj;
-      }
-      MsgEvent.emit("sendNewMsg", {
-        msgAuthor: "Server",
-        msgText: `${userObj.username} Connected`,
-        msgTime: new Date(),
-      });
+    const MSGS = await mongoClient
+      .db("ReactChatRoom")
+      .collection("Msgs")
+      .find()
+      .toArray();
+    const USERS = await mongoClient
+      .db("ReactChatRoom")
+      .collection("Users")
+      .find()
+      .toArray();
+    MsgEvent.on("sendInfo", async () => {
       const MSGS = await mongoClient
         .db("ReactChatRoom")
         .collection("Msgs")
@@ -50,57 +55,37 @@ chatRouter.get(
         .collection("Users")
         .find()
         .toArray();
-      MsgEvent.on("sendInfo", async () => {
-        const MSGS = await mongoClient
-          .db("ReactChatRoom")
-          .collection("Msgs")
-          .find()
-          .toArray();
-        const USERS = await mongoClient
-          .db("ReactChatRoom")
-          .collection("Users")
-          .find()
-          .toArray();
-        res.write(`data:${JSON.stringify({ msgs: MSGS, users: USERS })}\n\n`);
-      });
       res.write(`data:${JSON.stringify({ msgs: MSGS, users: USERS })}\n\n`);
+    });
+    res.write(`data:${JSON.stringify({ msgs: MSGS, users: USERS })}\n\n`);
 
-      req.on("close", async () => {
-        await mongoClient
-          .db("ReactChatRoom")
-          .collection("Users")
-          .deleteOne({ username: userObj.username });
-        MsgEvent.emit("sendNewMsg", {
-          msgAuthor: "Server",
-          msgText: `${userObj.username} Disconnected`,
-          msgTime: new Date(),
-        });
+    req.on("close", async () => {
+      await mongoClient
+        .db("ReactChatRoom")
+        .collection("Users")
+        .deleteOne({ username: userObj.username });
+      MsgEvent.emit("sendNewMsg", {
+        msgAuthor: "Server",
+        msgText: `${userObj.username} Disconnected`,
+        msgTime: new Date(),
       });
-    } catch (err) {
-      next({ status: 403, msg: "JWT invalid" });
-    }
+    });
+  } catch (err) {
+    next({ status: 403, msg: "JWT invalid" });
   }
-);
+});
 
-chatRouter.post(
-  "/new/msg",
-  checkAuthJWT,
-  (
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ): void => {
-    const { msgAuthor, msgText }: { [key: string]: string } = req.body;
-    if (!msgAuthor || !msgText) {
-      next({ status: 400, msg: "Need More Fields" });
-      return;
-    }
-    const userMsgObj = { msgAuthor, msgText, msgTime: new Date() };
-    console.log(userMsgObj, "MSGMSG 4TEST");
-
-    res.send("sucseesed");
-    MsgEvent.emit("sendNewMsg", userMsgObj);
+chatRouter.post("/new/msg", checkAuthJWT, (req, res, next): void => {
+  const { msgAuthor, msgText }: { [key: string]: string } = req.body;
+  if (!msgAuthor || !msgText) {
+    next({ status: 400, msg: "Need More Fields" });
+    return;
   }
-);
+  const userMsgObj = { msgAuthor, msgText, msgTime: new Date() };
+  console.log(userMsgObj, "MSGMSG 4TEST");
+
+  res.send("sucseesed");
+  MsgEvent.emit("sendNewMsg", userMsgObj);
+});
 
 export default chatRouter;
--- server/routes/user.ts
+++ server/routes/user.ts
@@ -6,54 +6,43 @@ import { mongoClient } from "../server";
 
 const userRouter = express.Router();
 
-userRouter.post(
-  "/login",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    //future more complex login
-    const { username } = req.body;
-    if (!username) {
-      next({ status: 400, msg: "Enter Username" });
-      return;
-    }
-    const JWTcookie: string = jwt.sign({ username: username }, JWTSALT, {
-      expiresIn: "1h",
-    });
-    res.cookie("JWT", JWTcookie, { maxAge: 1021031 });
-    res.send("Login Sucssued");
+userRouter.post("/login", (req, res, next) => {
+  //future more complex login
+  const { username } = req.body;
+  if (!username) {
+    next({ status: 400, msg: "Enter Username" });
+    return;
   }
-);
+  const JWTcookie: string = jwt.sign({ username: username }, JWTSALT, {
+    expiresIn: "1h",
+  });
+  res.cookie("JWT", JWTcookie, { maxAge: 1021031 });
+  res.send("Login Sucssued");
+});
 
-userRouter.post(
-  "/auth",
-  checkAuthJWT,
-  async (
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) => {
-    try {
-      const { authorization } = req.headers;
-      if (!authorization) {
-        throw authorization;
-      }
-      console.log(authorization, "auth");
-      const UserJWT = authorization.split(" ")[1];
-      const cookieUserObj = jwt.verify(UserJWT, JWTSALT);
-      if (typeof cookieUserObj === "string") {
-        throw cookieUserObj;
-      }
-      console.log(cookieUserObj, "cookieauth");
-
-      await mongoClient
-        .db("ReactChatRoom")
-        .collection("Users")
-        .insertOne(cookieUserObj);
-      res.send(cookieUserObj);
-    } catch (err) {
-      console.log(err);
-      next({ status: 444, msg: "unknown error" });
+userRouter.post("/auth", checkAuthJWT, async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      throw authorization;
     }
+    console.log(authorization, "auth");
+    const UserJWT = authorization.split(" ")[1];
+    const cookieUserObj = jwt.verify(UserJWT, JWTSALT);
+    if (typeof cookieUserObj === "string") {
+      throw cookieUserObj;
+    }
+    console.log(cookieUserObj, "cookieauth");
+
+    await mongoClient
+      .db("ReactChatRoom")
+      .collection("Users")
+      .insertOne(cookieUserObj);
+    res.send(cookieUserObj);
+  } catch (err) {
+    console.log(err);
+    next({ status: 444, msg: "unknown error" });
   }
-);
+});
 
 export default userRouter;
