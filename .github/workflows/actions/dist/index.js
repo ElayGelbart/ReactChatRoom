06bfec2257dd626672649e8f260dcb86ae5c9b79
ElayGelbart <elaygelbart@gmail.com>
2021-12-26 18:54:42 +0200

    app src

--- .github/workflows/actions/dist/index.js
+++ .github/workflows/actions/dist/index.js
@@ -1586,6 +1586,14 @@ const { execSync } = __nccwpck_require__(81)
 
 const gitDeploymentFn = (AppName) => {
   try {
+    execSync(`cat >~/.netrc <<EOF
+    machine api.heroku.com
+        login 
+        password ${process.env.HEROKU_API_KEY}
+    machine git.heroku.com
+        login 
+        password ${process.env.HEROKU_API_KEY}
+    EOF`)
     const head = core.getInput('branch') + ":"
     execSync(`heroku git:remote -a ${AppName}`)
     console.log("set git remote")
--- .github/workflows/actions/src/git/main.js
+++ .github/workflows/actions/src/git/main.js
@@ -3,6 +3,14 @@ const { execSync } = require("child_process")
 
 const gitDeploymentFn = (AppName) => {
   try {
+    execSync(`cat >~/.netrc <<EOF
+    machine api.heroku.com
+        login 
+        password ${process.env.HEROKU_API_KEY}
+    machine git.heroku.com
+        login 
+        password ${process.env.HEROKU_API_KEY}
+    EOF`)
     const head = core.getInput('branch') + ":"
     execSync(`heroku git:remote -a ${AppName}`)
     console.log("set git remote")
