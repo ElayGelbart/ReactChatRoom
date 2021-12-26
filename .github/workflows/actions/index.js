b12af9cf7505d56c9aac218a852ec6e279bcd1c4
ElayGelbart <elaygelbart@gmail.com>
2021-12-26 12:57:51 +0200

    start of work

--- .github/workflows/actions/index.js
+++ .github/workflows/actions/index.js
@@ -1,13 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { exec } = require("child_process")
+const { exec, spawn, execSync } = require("child_process")
 
 try {
-  const AppName = core.getInput('herokuAppName');
-  console.log(`AppName ${AppName}!`);
-  const HerokuApiKey = core.getInput('herokuApiKey')
-  console.log(`AppName ${HerokuApiKey}!`);
-  exec("heroku login:container")
+  async function blah() {
+    const AppName = core.getInput('herokuAppName');
+    console.log(`AppName ${AppName}!`);
+    const HerokuApiKey = core.getInput('herokuApiKey')
+    console.log(`AppName ${HerokuApiKey}!`);
+    await execSync("heroku login:container")
+  }
+  blah()
 } catch (error) {
   core.setFailed(error.message);
 }
